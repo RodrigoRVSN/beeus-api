6783edce01eca62f46e6cd43238e6fad029fbcda
rodrigorvsn <rodrigovictor81@gmail.com>
2023-03-17 11:36:04 -0300

    feat: add validation in name email and password fields

--- src/models/users/entities/user.go
+++ src/models/users/entities/user.go
@@ -8,10 +8,10 @@ import (
 
 type User struct {
 	gorm.Model
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
+	Name      string    `json:"name" validate:"required"`
+	Email     string    `json:"email" validate:"required,email"`
 	AvatarUrl string    `json:"avatar_url" gorm:"default:https://avatarfiles.alphacoders.com/260/thumb-260445.jpg"`
-	Password  string    `json:"password"`
+	Password  string    `json:"password" validate:"required"`
 	Points    int       `json:"points" gorm:"default:0"`
 	CreatedAt time.Time `json:"created_at" gorm:"default:0"`
 }
