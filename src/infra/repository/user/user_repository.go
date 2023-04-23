66e7d25e678896a9299b05ba84a30f9396799e87
rodrigorvsn <rodrigovictor81@gmail.com>
2023-04-23 17:56:12 -0300

    refactor: implement repository interface

--- src/infra/repository/user/user_repository.go
+++ src/infra/repository/user/user_repository.go
@@ -1,6 +1,8 @@
 package userRepository
 
 import (
+	"github.com/rodrigoRVSN/beeus-api/src/application/dto"
+	"github.com/rodrigoRVSN/beeus-api/src/domain/entity"
 	"gorm.io/gorm"
 )
 
@@ -8,6 +10,12 @@ type UserRepository struct {
 	DB *gorm.DB
 }
 
-func NewUserRepository(db *gorm.DB) *UserRepository {
+type UserRepositoryImplementation interface {
+	CreateUser(payload *entity.User) error
+	SignInUser(payload dto.SignInInputDTO) (*dto.SignInOutputDTO, error)
+	FindAllUsers(user *entity.User) ([]entity.User, error)
+}
+
+func NewUserRepository(db *gorm.DB) UserRepositoryImplementation {
 	return &UserRepository{DB: db}
 }
