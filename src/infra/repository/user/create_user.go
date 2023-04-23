b2a6b2f60eb00d114c83454bcb249d8a5da0f10f
rodrigorvsn <rodrigovictor81@gmail.com>
2023-04-23 18:11:00 -0300

    feat: verify if the user email already exists

--- src/infra/repository/user/create_user.go
+++ src/infra/repository/user/create_user.go
@@ -1,17 +1,29 @@
 package userRepository
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/rodrigoRVSN/beeus-api/src/domain/entity"
 	hash "github.com/rodrigoRVSN/beeus-api/src/infra/helpers"
+	"gorm.io/gorm"
 )
+
+func (r *UserRepository) checkUserAlreadyExists(payload *entity.User, email string) error {
+	if err := r.DB.First(payload, "email = ?", email).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil
+		}
+		return err
+	}
+	return fmt.Errorf("houve um erro ao cadastrar o email")
+}
 
 func (r *UserRepository) CreateUser(payload *entity.User) error {
 	payload.Password = hash.HashPassword(payload.Password)
 
-	if userFound := r.DB.First(&payload, "email = ?", payload.Email); userFound != nil {
-		return fmt.Errorf("houve um erro ao cadastrar o email")
+	if err := r.checkUserAlreadyExists(payload, payload.Email); err != nil {
+		return err
 	}
 
 	response := r.DB.Create(payload)
