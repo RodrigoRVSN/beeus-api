bdc7173367156aa92f0a14ac41aefd07d7eb2511
rodrigorvsn <rodrigovictor81@gmail.com>
2023-05-05 21:50:16 -0300

    fix: pass entity in delete documentation repository and id

--- src/infra/repository/documentation/delete_documentation.go
+++ src/infra/repository/documentation/delete_documentation.go
@@ -2,10 +2,11 @@ package documentationRepository
 
 import (
 	documentationDTO "github.com/rodrigoRVSN/beeus-api/src/application/dto/documentation"
+	"github.com/rodrigoRVSN/beeus-api/src/domain/entity"
 )
 
 func (r *DocumentationRepository) DeleteDocumentation(documentation *documentationDTO.FindDocumentationByIdOutputDTO) error {
-	err := r.DB.Delete(documentation).Error
+	err := r.DB.Delete(&entity.Documentation{}, documentation.Id).Error
 
 	return err
 }
--- src/infra/repository/documentation/find_documentation_by_id.go
+++ src/infra/repository/documentation/find_documentation_by_id.go
@@ -6,13 +6,14 @@ import (
 	documentationDTO "github.com/rodrigoRVSN/beeus-api/src/application/dto/documentation"
 	userDTO "github.com/rodrigoRVSN/beeus-api/src/application/dto/user"
 	"github.com/rodrigoRVSN/beeus-api/src/domain/entity"
+	"gorm.io/gorm"
 )
 
 func (r *DocumentationRepository) FindDocumentationById(documentationId uint) (*documentationDTO.FindDocumentationByIdOutputDTO, error) {
 	documentation := entity.Documentation{}
 
-	if err := r.DB.Preload("Author").Find(&documentation).Error; err != nil {
-		if err.Error() == "record not found" {
+	if err := r.DB.Preload("Author").First(&documentation, documentationId).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
 			return nil, fmt.Errorf("documentação não encontrada")
 		}
 		return nil, err
