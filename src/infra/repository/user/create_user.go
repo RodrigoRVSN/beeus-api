50fce531994944818426d61fb03436deceb2722c
rodrigorvsn <rodrigovictor81@gmail.com>
2023-05-20 13:49:46 -0300

    refactor: move get_user_by_email to own file

--- src/infra/repository/user/create_user.go
+++ src/infra/repository/user/create_user.go
@@ -1,22 +1,10 @@
 package userRepository
 
 import (
-	"errors"
-
 	userDTO "github.com/rodrigoRVSN/beeus-api/src/application/dto/user"
 	"github.com/rodrigoRVSN/beeus-api/src/domain/entity"
 	hash "github.com/rodrigoRVSN/beeus-api/src/infra/helpers"
 )
-
-func (r *UserRepository) CheckIfUserAlreadyExists(email string) error {
-	user := entity.User{}
-
-	result := r.DB.Where("email = ?", email).First(&user)
-	if result.RowsAffected > 0 {
-		return errors.New("houve um erro ao cadastrar o email")
-	}
-	return nil
-}
 
 func (r *UserRepository) CreateUser(payload userDTO.SignUpInputDTO) error {
 	payload.Password = hash.HashPassword(payload.Password)
