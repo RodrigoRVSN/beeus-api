1d6ab6ddf658b97c9668471b80385d2cad8b3477
rodrigorvsn <rodrigovictor81@gmail.com>
2023-05-20 16:23:33 -0300

    refactor: abstract fiber context

--- src/application/controller/documentation/get_documentation.go
+++ src/application/controller/documentation/get_documentation.go
@@ -4,20 +4,21 @@ import (
 	"strconv"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/rodrigoRVSN/beeus-api/src/infra/context"
 )
 
-func (controller *DocumentationController) GetDocumentation(context *fiber.Ctx) error {
-	documentationIDStr := context.Params("documentationId")
+func (controller *DocumentationController) GetDocumentation(ctx context.Context) error {
+	documentationIDStr := ctx.GetParam("documentationId")
 	documentationID, err := strconv.ParseUint(documentationIDStr, 10, 64)
 
 	if err != nil {
-		return context.Status(fiber.StatusBadRequest).SendString("Invalid documentation ID")
+		return ctx.SendJson(fiber.StatusBadRequest, fiber.Map{"status": "fail", "message": "ID de documentação inválido"})
 	}
 
 	documentation, err := controller.useCase.GetDocumentation(uint(documentationID))
 	if err != nil {
-		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+		return ctx.SendJson(fiber.StatusBadRequest, fiber.Map{"status": "fail", "message": err.Error()})
 	}
 
-	return context.Status(fiber.StatusOK).JSON(documentation)
+	return ctx.SendJson(fiber.StatusOK, documentation)
 }
--- src/infra/router/documentation_routes.go
+++ src/infra/router/documentation_routes.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	documentationController "github.com/rodrigoRVSN/beeus-api/src/application/controller/documentation"
+	"github.com/rodrigoRVSN/beeus-api/src/infra/context"
 	"github.com/rodrigoRVSN/beeus-api/src/infra/middleware"
 )
 
 func DocumentationRoutes(app *fiber.App, documentationController *documentationController.DocumentationController) {
-	app.Get("/documentation", documentationController.ListAllDocumentations)
-	app.Get("/documentation/:documentationId", documentationController.GetDocumentation)
-	app.Post("/documentation", middleware.AuthMiddleware, documentationController.CreateDocumentation)
-	app.Delete("/documentation/:documentationId", middleware.AuthMiddleware, documentationController.DeleteDocumentation)
-	app.Put("/documentation/:documentationId", middleware.AuthMiddleware, documentationController.EditDocumentation)
+	app.Get("/documentation", context.AdaptHandler(documentationController.ListAllDocumentations))
+	app.Get("/documentation/:documentationId", middleware.AuthMiddleware, context.AdaptHandler(documentationController.GetDocumentation))
+	app.Post("/documentation", middleware.AuthMiddleware, context.AdaptHandler(documentationController.CreateDocumentation))
+	app.Delete("/documentation/:documentationId", middleware.AuthMiddleware, context.AdaptHandler(documentationController.DeleteDocumentation))
+	app.Put("/documentation/:documentationId", middleware.AuthMiddleware, context.AdaptHandler(documentationController.EditDocumentation))
 }
